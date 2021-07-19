8c6276348a330c9d98a564462c2aa5223568b879
Ayman <yaoiotakusama@gmail.com>
2021-07-20 00:38:20 +0530

    Rock Paper Scissors with UI has been implemented

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A rock-paper-paper implementation in JS, done as part of my course in The Odin Project.
+A rock-paper-paper implementation in vanilla JS, HTML and CSS, done as part of my course in The Odin Project.
---
+++ app.js
@@ -0,0 +1,73 @@
+function computerSelection() {
+    const choices = ["Rock","Paper","Scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelect, computerSelect) {
+    if(playerSelect === computerSelect) {
+        return -1;
+    }
+
+    if(playerSelect === "Rock") {
+        if(computerSelect === "Scissors") {
+            return 1;
+        }else {
+            return 0;
+        }
+    }else if(playerSelect === "Scissors") {
+        if(computerSelect === "Paper") {
+            return 1;
+        }else {
+            return 0;
+        }
+    }else {
+        if(computerSelect === "Paper") {
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+}
+
+function game(playerChoice) {
+    let playerCount = 0;
+    let computerCount = 0;
+    while(playerCount < 5 && computerCount < 5) {
+        let playerSelect = playerSelection();
+    }
+}
+let playerCount = 0;
+let computerCount = 0;
+
+const buttons = document.querySelectorAll("button");
+
+console.log(buttons);
+
+buttons.forEach(button => {
+    button.addEventListener("click" ,() => {
+
+        if(playerCount < 5 && computerCount < 5) {
+            let playerSelect = button.id;
+            let computerSelect = computerSelection();
+
+            let temp = playRound(playerSelect,computerSelect);
+
+            if(temp != -1) {
+                playerCount += temp;
+                computerCount += (1 - temp);
+                document.querySelector(".player-counter").innerText = "Player Count : " + playerCount;
+                document.querySelector(".computer-counter").innerText = "Computer Count: " + computerCount;
+                document.querySelector(".player-image").firstElementChild.setAttribute("src","images/" + playerSelect + ".svg");
+                document.querySelector(".computer-image").firstElementChild.setAttribute("src","images/" + computerSelect + ".svg");
+            }else {
+                document.querySelector(".player-image").firstElementChild.setAttribute("src","images/" + playerSelect + ".svg");
+                document.querySelector(".computer-image").firstElementChild.setAttribute("src","images/" + computerSelect + ".svg");
+            }
+        }
+        if(playerCount == 5) {
+            alert("You Win!");
+        }else if(computerCount == 5){
+            alert("You Lose!");
+        }
+    });
+});
